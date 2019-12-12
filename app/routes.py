e43d11184933f27281ddfb97efc3451bc3ad7e8d
duoduolee <lishaolint@gmail.com>
2019-12-12 11:00:20 +0800

    add the templates for the flask application 0.2 version

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, this is the miguel flask website!"
+    theuser = {'username': 'Duoduo'}
+    postnames = [
+        {
+            'author': {'username': 'duoduo'},
+            'body': 'the principle of the world and nature'
+        },
+        {
+            'author': {'username': 'JK.rolin'},
+            'body': 'The harry port and magic stone'
+        },
+        {
+            'author': {'username': 'Jane.Osting'},
+            'body': 'Pride and Prejudice'
+        }
+    ]
+
+    return render_template('index.html', title='flask miguel',user=theuser,posts=postnames)
+
+
+
+
 

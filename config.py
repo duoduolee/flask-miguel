0767cfb013bf3e45686e66ccbc02649df1a5e305
duoduolee <lishaolint@gmail.com>
2019-12-13 23:51:12 +0800

    add the database and migration for the blog

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'the-key-of-the-flask-miguel'
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
